0bdace964e23f9437c34f788c2be236a8920272a
NikitaKS <pancxxx2@gmail.com>
2020-04-01 10:55:27 +0300

    Updates

---
+++ precache-manifest.abd4d863dfd312daad197024d2dfff62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddb6c8412976c46c4815e29591ab69a",
+    "url": "/car-house/index.html"
+  },
+  {
+    "revision": "896b171fff465ab49bf7",
+    "url": "/car-house/static/css/main.83f877d4.chunk.css"
+  },
+  {
+    "revision": "7e4ae247fded0f87113f",
+    "url": "/car-house/static/js/2.221dc2e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/car-house/static/js/2.221dc2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896b171fff465ab49bf7",
+    "url": "/car-house/static/js/main.48c5bccc.chunk.js"
+  },
+  {
+    "revision": "9c6c6e2d325c3e5e7ac9",
+    "url": "/car-house/static/js/runtime-main.a4c064c4.js"
+  }
+]);
